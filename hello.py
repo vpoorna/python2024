4be4d11df8854ab1561b085595805a2b77335168
vpoorna@gmail.comgit -vexitexitenter <vpoorna@gmail.com>
2024-01-20 19:28:47 +0530

    myhw

---
+++ hello.py
@@ -0,0 +1,20 @@
+x=0
+y=1
+print (x)
+print (y)
+a = int(input("Enter a number: "))
+print(a)        
+b = (input("Enter your name: "))
+print(b)
+print(b[0])
+print(b[1])
+print(b[-1])
+print(b[1:4])
+proverb = "Practice makes man perfect"
+length = len(proverb)
+contains_perfect = "perfect" in proverb 
+repeated_proverb = proverb * 3
+print (proverb)
+print (length)
+print (contains_perfect)
+print (repeated_proverb)
